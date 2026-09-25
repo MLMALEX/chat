b1f1f31626039d574e727e6845064f2572191c79
MLMALEX <85957190+MLMALEX@users.noreply.github.com>
2026-09-25 16:46:57 +0300

    Register call endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CallController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::post('/calls', [CallController::class, 'start'])->name('calls.start');
+    Route::post('/calls/{call}/respond', [CallController::class, 'respond'])->name('calls.respond');
+    Route::post('/calls/{call}/end', [CallController::class, 'end'])->name('calls.end');
+    Route::post('/calls/{call}/token', [CallController::class, 'token'])->name('calls.token');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
